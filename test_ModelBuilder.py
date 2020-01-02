fc3f6a124ee7b133b09608a7dac5c51b9ea26487
ysaule <yvan.saule@digitaslbi.com>
2020-01-02 17:42:19 +0100

    adding some test for CI

---
+++ test_ModelBuilder.py
@@ -0,0 +1,9 @@
+import ModelBuilder
+import ImageSize
+
+class TestModelBuilder:
+
+    def test_build(self):
+        net = ModelBuilder()
+        model = net.build(width=ImageSize.WIDTH, height=ImageSize.HEIGHT, depth=3, classes=2)
+        assert 11 == len(model.layers())
