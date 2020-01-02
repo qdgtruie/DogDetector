7518958370167cb01cbdc3d935440dfb37cca956
ysaule <yvan.saule@digitaslbi.com>
2020-01-02 18:05:04 +0100

    fixing tests

--- test_ModelBuilder.py
+++ test_ModelBuilder.py
@@ -6,4 +6,4 @@ class TestModelBuilder:
     def test_build(self):
         net = Builder()
         model = net.build(width=ImageSize.WIDTH, height=ImageSize.HEIGHT, depth=3, classes=2)
-        assert 11 == len(model.layers())
+        assert 11 == len(model.layers)
